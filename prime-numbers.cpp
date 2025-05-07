77522beeb0415bacaaeb7983ad809e6eb551fe09
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-07 10:04:41 -0300

    feat: exercicio de numeros primos

---
+++ prime-numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool checkIsPrime(int numberToCheck){
+    for (int i = 2; i < numberToCheck; i++) {
+         if (numberToCheck % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for(int numberToCheck = 2; numberToCheck < N; numberToCheck++){
+        bool isPrime = checkIsPrime(numberToCheck);
+        if(isPrime){
+            cout << numberToCheck << " ";
+        }
+    }
+    return 0;
+}
