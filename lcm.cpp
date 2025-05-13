358c049ec38a6b21af3ac9b2cb5139466ad47071
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-13 15:43:12 -0300

    style= identaçao

--- lcm.cpp
+++ lcm.cpp
@@ -4,38 +4,46 @@ using namespace std;
 
 int main()
 {
-    int arr[2] = {30, 60};
-    int arr_length = sizeof(arr) / sizeof(arr[0]);
-    int count = 2;
+  int arr[2] = {30, 60};
+  int arr_length = sizeof(arr) / sizeof(arr[0]);
+  int count = 2;
 
-    while(true){
-       bool finished = true;
+  while (true)
+  {
+    bool finished = true;
 
-       for(int index = 0; index < arr_length; index++){
-        if(arr[index] % count == 0){
-          arr[index] /= count;
-          finished = false;
-        }
-       }
-
-       if (finished) {
-        count++;
-       } else {
-        cout << count << " ";
-        count = 2;
-       }
-
-       bool allOne = true;
-       for(int i = 0; i< arr_length; i++){
-         if(arr[i] != 1){
-            allOne= false;
-            break;
-         }
-       }
-
-       if(allOne) break;
+    for (int index = 0; index < arr_length; index++)
+    {
+      if (arr[index] % count == 0)
+      {
+        arr[index] /= count;
+        finished = false;
+      }
     }
 
+    if (finished)
+    {
+      count++;
+    }
+    else
+    {
+      cout << count << " ";
+      count = 2;
+    }
 
-    return 0;
+    bool allOne = true;
+    for (int i = 0; i < arr_length; i++)
+    {
+      if (arr[i] != 1)
+      {
+        allOne = false;
+        break;
+      }
+    }
+
+    if (allOne)
+      break;
+  }
+
+  return 0;
 }
--- prime-numbers.cpp
+++ prime-numbers.cpp
@@ -2,9 +2,11 @@
 
 using namespace std;
 
-bool checkIsPrime(int numberToCheck){
-    for (int i = 2; i < numberToCheck; i++) {
-         if (numberToCheck % i == 0)
+bool checkIsPrime(int numberToCheck)
+{
+    for (int i = 2; i < numberToCheck; i++)
+    {
+        if (numberToCheck % i == 0)
             return false;
     }
 
@@ -16,9 +18,11 @@ int main()
     int N;
     cin >> N;
 
-    for(int numberToCheck = 2; numberToCheck < N; numberToCheck++){
+    for (int numberToCheck = 2; numberToCheck < N; numberToCheck++)
+    {
         bool isPrime = checkIsPrime(numberToCheck);
-        if(isPrime){
+        if (isPrime)
+        {
             cout << numberToCheck << " ";
         }
     }
