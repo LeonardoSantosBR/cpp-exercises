b609e03ce8266aef0aa4f3e3129d606d3e7126d8
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-16 11:24:53 -0300

    feat: mdc

---
+++ gcf.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int result = 1;
+  int count = 2;
+  vector<int> ns = {30, 60};
+
+  while (true)
+  {
+    bool reduced = false;
+    for (int i = 0; i < ns.size(); i++)
+    {
+      if (ns[i] % count == 0)
+      {
+        ns[i] /= count;
+        reduced = true;
+      }
+
+      if (reduced && ns[i + 1] % count == 0)
+      {
+        ns[1] /= count;
+        result *= count;
+      }
+    }
+
+    if (!reduced)
+    {
+      count++;
+    }
+    else
+    {
+      count = 2;
+    }
+
+    bool isAllOne = true;
+    for (int i = 0; i < ns.size(); i++)
+    {
+      if (ns[i] != 1)
+      {
+        isAllOne = false;
+        break;
+      }
+    }
+
+    if (isAllOne)
+      break;
+  }
+
+  return 0;
+}
