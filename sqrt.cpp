ae9ffe33984c22e4c301b76a9479a637818f973e
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-20 14:37:26 -0300

    feat: raiz quadrada

---
+++ sqrt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  //raiz quadrada
+  double N;
+  cin >> N;
+
+  cout << sqrt(N) << endl;
+  return 0;
+}
