316580f14d7aecab8f58c0600db7a981a12dcfca
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-05-23 13:22:43 -0300

    refactor: numero de fibonacci ajustado

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -19,3 +19,23 @@ int main()
 
     return 0;
 }
+
+// * leetcode , mais inteligente, utiliza a variavel c como temporaria e depois introduz
+//   na variavel b.atualizando a e b consecutivamente.
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n == 0)
+//             return 0;
+//         if (n == 1)
+//             return 1;
+
+//         int a = 0, b = 1, c;
+//         for (int i = 2; i <= n; i++) {
+//             c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//         return b;
+//     }
+// };
