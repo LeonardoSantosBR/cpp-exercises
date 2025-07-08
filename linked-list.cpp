7225117c01b7eaf99cdf96c49c47814cdc961567
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-07-08 11:33:55 -0300

    feat: linkedlist ajustes

--- linked-list.cpp
+++ linked-list.cpp
@@ -4,22 +4,43 @@ using namespace std;
 struct ListNode
 {
     int val;
-    ListNode* next;
+    ListNode *next;
 
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
 int main()
 {
-    ListNode* firstNode = new ListNode(10);
-    ListNode* secondNode = new ListNode(5);
-    ListNode* thirdNode = new ListNode(23);
-    ListNode* fourthNode = new ListNode(5);
+    // criando os nós
+    ListNode *firstNode = new ListNode(1); //head
+    ListNode *secondNode = new ListNode(2);
+    ListNode *thirdNode = new ListNode(5);
+    ListNode *fourthNode = new ListNode(3);
+    ListNode *fifthNode = new ListNode(5);
 
+    // criando ciclo
     firstNode->next = secondNode;
     secondNode->next = thirdNode;
     thirdNode->next = fourthNode;
-    fourthNode->next= firstNode;
-    
+    fourthNode->next = fifthNode;
+    fifthNode->next = secondNode;
+
+    // algoritmo floyd - tartaruga e lebre
+    ListNode *slow = firstNode;
+    ListNode *fast = firstNode;
+
+    bool thereIsCyle = false;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            thereIsCyle = true;
+            return;
+        };
+    }
+
     return 0;
 }
